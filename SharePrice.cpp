ae6d8fc5f21ea31f43d0e7b5dc68d87d0bee6d9c
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2019-11-26 10:10:01 +0800

    股市的類別（範例）

---
+++ SharePrice.cpp
@@ -0,0 +1,42 @@
+ // 2019, 1126, 10:00, by Queenie
+ // Class & Access Control & Data Hidden
+ 
+ class ShareMarket{
+
+//            此處為封裝區 Encapsulation
+//           /
+//          /
+//         /
+//        /
+//       /
+   private:
+        char target[10];
+        int shares;
+        float share_val;
+        double total_val;
+        void know_totoal(){
+          total_val= shares * share_val;
+        }
+
+
+        //      實作區 （成員函數）
+        //     /
+        //    /
+        //   /
+        //  /
+        // /
+   public:
+        void before(const char *com, int n, double pr);
+        void buy(int shareAmount, double dealPrice);
+        void sell(int shareAmount, double dealPrice);
+        void after(double dealPrice);
+        void show();
+                //    \
+                //     \
+                //      \ 
+                //       \
+                //        \ 
+                //         此處作抽象化處理 Abstract 
+
+
+};
